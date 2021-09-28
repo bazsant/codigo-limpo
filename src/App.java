80454b2376aa0d751661a989353b8adab503cf6c
Jhones Bazilio dos Santos <jhonesbdsantos@outlook.com.br>
2021-09-27 21:03:11 -0300

    commit inicial

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        GuessStatisticsMessage test = new GuessStatisticsMessage();
+        System.out.println(test.make('r', 2));
+    }
+}
